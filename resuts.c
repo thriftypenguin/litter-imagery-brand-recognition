a85c98a13a96450b7d6498c55176af5896fc25d4
ArtemChemist <lebedevfedora@gmail.com>
2024-07-07 21:38:55 -0400

    evaluate the model

---
+++ resuts.c
@@ -0,0 +1,298 @@
+file_name	labels	predicted_brands	correct	cases
+wS5k4Dg9Dj7WFd1UTtRQLZ3CPCXcmEIRYFARqYIo	"5-hour energy,"	nan	0	1
+IMG_6080	"99,"	nan	0	1
+rdwPeYBeIEmAabqjetu492CjBJnopMnV4UDX0PQg	"acadia,"	nan	0	1
+rqpAFyqrDuLfjuYNW1aEcyBqnGTMqaCBvewR8yNM	"arizona tea,"	arizona	1	1
+yn7eItUfYQKbFacIGYQlGfzCQHtPvqIZhwJq4Zk7	"bang,"	nan	0	1
+eUseyGr0vkAqahUMWSpVUOcQBfFRm40PMV0cBvnf	"big gulp,"	nan	0	1
+IMG_3450	"boulevard brewing,"	nan	0	1
+IMG_0579	"brisk,"	nan	0	1
+cEZ9zgCWK2hsITcvW61WAQeavf2eAP2pX4G2fNEI	"bud,"	bud_light	1	1
+CKYAwFX2T04wSveFh0n5QxCWCtWaz3CDoiadR1ek	"bud,"	"bud_light, bud_light"	1	1
+IMG_3866	"bud,"	bud_light	1	1
+PoZkQE3TPKMrUmLz3sEEz6kNMhD6i2Ef3VLtSRKQ	"bud,"	nan	0	1
+IMG_2834	"bud,bud,"	nan	0	2
+IMG_5641	"bud,bud,"	"nan, bud_light, bud_light"	2	2
+NvFgwLd3NY8nZVs65rhDuhiQcBKLUFFASnU4oIKE	"bud,bud,bud,"	bud_light	1	3
+Cunf7NlDwiyY17bsPC87QUuBlrPpOsESfUFqNAkZ	"bud,bud,corona,corona,"	"nan, nan, nan"	0	4
+IMG_3250	"bud,heineken,"	"bud_light, heineken, heineken"	2	2
+IMG_7353	"budweiser,"	nan	0	1
+IMG_6330	"burger king,"	nan	0	1
+IMG_0881	"camel,"	heineken	0	1
+3IIxk6EMH9n7Un7xYwsPtTCwBPJM4e16TkiehifB	"cheetos,"	"nan, nan, nan"	0	1
+nUMK0KFKt1cBXBokYIliij3sXUKAOXcMr517O6mr	"cheetos,"	cheetos	1	1
+aSAMh5W26GOvhTNa8c0X68Zy4eJx9MhP6YY5vEMC	"coca cola,"	dr_pepper	0	1
+IMG_0822	"coca cola,"	"nan, poland_spring, heineken"	0	1
+IMG_1312	"coca cola,"	"modelo, coca_cola"	1	1
+IMG_2559	"coca cola,"	coca_cola	1	1
+IMG_3557	"coca cola,"	coca_cola	1	1
+IMG_4357	"coca cola,"	nan	0	1
+IMG_8991	"coca cola,"	coca_cola	1	1
+IMG_9387	"coca cola,"	nan	0	1
+YsdfiPNKjAdJVORmgqqp3QUdS5mSQ149DSZagBLc	"coca cola,coca cola,"	nan	0	2
+eUNF3jvQp86MjNVVxuYCv8dC9dQyEvmYe80Icae9	"coors,"	nan	0	1
+IMG_4115	"coors,"	"nan, corona, corona"	0	1
+FxgtEPtTdVkdfrIYEf4CClUEwVvY5SU0wlBi5znv	"corona,"	nan	0	1
+gKSZAvv4gPwceEXawFHq7mEgMdFnQlYdd6eYkubn	"corona,"	corona	1	1
+IMG_0001	"corona,"	nan	0	1
+IMG_0957	"corona,"	"heineken, corona, corona"	1	1
+IMG_2966	"corona,"	corona	1	1
+IMG_4294	"corona,"	corona	1	1
+IMG_4797	"corona,"	nan	0	1
+IMG_5065	"corona,"	"heineken, nan"	0	1
+IMG_6715	"corona,"	nan	0	1
+IMG_7540	"corona,"	"busch_light, corona"	1	1
+IMG_7892	"corona,"	nan	0	1
+IMG_8851	"corona,"	nan	0	1
+IMG_9102	"corona,"	corona	1	1
+2R7Sp85dVjjkaxudOXW79jnZqqDKkFtx4vnaNhTC	"corona,corona,"	nan	0	2
+gMyVYZ2G2PM9Z8QlQc4Wt2QmwJI1bXHIZd6zEIIO	"corona,corona,corona,corona,corona,corona,corona,corona,corona,corona,corona,corona,corona,heineken,heineken,heineken,heineken,heineken,heineken,heineken,gatorade,"	"heineken, heineken, corona"	3	21
+IMG_6011	"corona,corona,corona,heineken,"	"heineken, modelo, heineken"	1	4
+YC5LRS6BcKz5QfL2HB3EB5tYxeAlpwA9pTED4TuI	"corona,corona,heineken,heineken,bud,bud,stella artois,stella artois,camel,camel,jarritos,miller,sangria senorial,"	nan	0	13
+IMG_2968	"corona,deer park,turkey hill,"	"modelo, nan"	0	3
+IMG_3987	"corona,heineken,"	nan	0	2
+tZPmjcPo7sLFRkn8ZdxfvGS1cWxx2R2JodOmfZKd	"corona,heineken,heineken,heineken,"	"heineken, heineken, corona"	3	4
+4f8GFf4yPJNrquJd2bZrbkS0No5S49IkkaZVtnMg	"corona,kirkland,"	nan	0	2
+phSqAmgkUcg125Aq0nswzHMZtFJtlq0OwVWrFtAk	"corona,modelo,perrier,"	"corona, nan, modelo"	2	3
+IMG_6557	"dasani,"	nan	0	1
+IMG_2599	"dasani,heineken,"	"nan, nan, corona"	0	2
+IMG_5806	"dasy,"	nan	0	1
+tTzh0I2nNTMsC5QXKatN6B9jhqO3oiJfiIeGHZO4	"david,"	nan	0	1
+AVCxXxrrWcU83HHx8YGwlV9eDE0Uzo6tLfKR0pKT	"deer park,"	nan	0	1
+IMG_1689	"deer park,"	"deer_park, nan"	1	1
+IMG_8736	"deer park,"	deer_park	1	1
+IMG_9844	"deer park,"	nan	0	1
+iQGiuuoAbGpQWOX1NpBbr4fOQw7WBH1yxo9P6mE0	"deer park,"	nan	0	1
+SIuaqMGYcswD4im0NFYDMEZumRdhNY1AMqVD6ytO	"deer park,"	deer_park	1	1
+GeD4DMMUJxBugDKeF1Z61VAmWVDiMPvPFl1p27s8	"deer park,corona,deer park,heineken,cheetos,coca cola,"	"nan, nan, coca_cola, nan, corona, heineken, nan, deer_park"	4	6
+IMG_0538	"deer park,goya,goya,"	nan	0	3
+IMG_6905	"dunkin donuts,"	"heineken, heineken, heineken, heineken"	0	1
+qFNX4g7PJI2ktCsCB8QLKC0tdR6pTRSAMr39Kdmd	"evan williams,"	nan	0	1
+IMG_3492	"fanta,"	nan	0	1
+BnCIz5FgwWtPcMXZsp8uLT4QLFv1GT2IgOiznYER	"fireball,bill miller,hill country fair,"	"nan, nan"	0	3
+IMG_0650	"flow,"	"nan, heineken, heineken, heineken, heineken"	0	1
+IMG_2179	"fruit punch,mountain dew,milwakee best ice,"	"heineken, busch_light"	0	3
+IMG_8472	"gatorade,"	nan	0	1
+QFzSPOKzlPHSENTcTqUW8D8vy4Hs8tTxm95mlgYz	"gatorade,"	nan	0	1
+IMG_6477	"gatorade,corona,heineken,"	"nan, nan, gatorade"	1	3
+p988w46JuP38cQ23cbHWHnCPrhoSb2tWYc6uC3Wl	"gogo,"	"reeses, nan"	0	1
+xobEi5pnDCsVEzO1GChcLNw9d26TzYBaQOzyNz6Y	"grace,"	nan	0	1
+EUnSBrXf5bH61ZKoCFcZaJq4dVxe5HVnJRqb4YDq	"green mountain,"	green_mountain	1	1
+iCAoavgtbEEa4qc99vPkSBOZ7kvxm4QjpSgJGwpo	"heineken,"	heineken	1	1
+IMG_0307	"heineken,"	"heineken, heineken"	1	1
+IMG_0328	"heineken,"	heineken	1	1
+IMG_0862	"heineken,"	"heineken, stella"	1	1
+IMG_2104	"heineken,"	heineken	1	1
+IMG_3174	"heineken,"	"heineken, heineken"	1	1
+IMG_3874	"heineken,"	heineken	1	1
+IMG_5118	"heineken,"	heineken	1	1
+IMG_5203	"heineken,"	"nan, nan, nan, heineken"	1	1
+IMG_5256	"heineken,"	"heineken, heineken"	1	1
+IMG_5416	"heineken,"	stella	0	1
+IMG_8563	"heineken,"	"nan, nan, heineken"	1	1
+IMG_9338	"heineken,"	heineken	1	1
+lK2KpkydotGnaOOwlPHGGVhxYsed9iuJK9aRH36Y	"heineken,"	"heineken, heineken, heineken"	1	1
+m20N59uFdCW5imcXN14wzw3UQPvoYJ7waFeO3mvE	"heineken,aw,busch,dasani,"	"busch_light, heineken, nan, nan, nan"	2	4
+IMG_3241	"heineken,corona,"	"corona, nan, heineken"	2	2
+IMG_2309	"heineken,heineken,"	"heineken, heineken"	2	2
+IMG_8966	"heineken,heineken,"	"heineken, heineken, heineken, heineken"	2	2
+IMG_9035	"heineken,heineken,heineken,heineken,heineken,"	"heineken, heineken"	2	5
+IMG_0692	"heineken,heineken,heineken,heineken,heineken,heineken,heineken,heineken,heineken,heineken,"	"heineken, heineken, heineken, heineken, heineken, heineken"	6	10
+sA2lWliS5hEclOMtiHR8BfNvMFvtxI6BT2oOW7A1	"heineken,heineken,heineken,heineken,heineken,heineken,heineken,heineken,heineken,heineken,heineken,heineken,heineken,"	"heineken, heineken, heineken, heineken, heineken, heineken, heineken, heineken, heineken, heineken, heineken, heineken, heineken, heineken, heineken, heineken"	13	13
+zwWCqQuDCnsNEJDXu7APjIqNN78LQ7fCq7As3RJu	"heineken,heineken,modelo,arizona tea,coca cola,sprite,natural ice,milwakee best ice,budweiser,milwakee best ice,"	"heineken, heineken, sprite"	3	10
+HjvPpfAeK29UAhoBd8ri6anf3pb0GgDol8OhQd8f	"heineken,popeyes,"	heineken	1	2
+IMG_0094	"hersheys,"	"caprisun, nan, nan"	0	1
+IMG_9959	"icehouse edge,"	nan	0	1
+IVkfQ4dWjihJZ0NzxnVUfXaMXkJR5eTRSU2mByWN	"jim beam,"	nan	0	1
+IMG_1189	"kirkland,"	kirkland	1	1
+IMG_4260	"kirkland,"	nan	0	1
+IMG_4646	"kirkland,"	nan	0	1
+IMG_7378	"kirkland,"	heineken	0	1
+IMG_8373	"kirkland,"	"kirkland, nan"	1	1
+IMG_8104	"kit kat,"	nan	0	1
+IMG_2672	"la yogurt,"	"3_musketeers, nan"	0	1
+IMG_1245	"lacroix,"	"nan, nan"	0	1
+IMG_1759	"lays,"	"nan, nan, nan"	0	1
+bwsInOXC4CHjLUbgNOwNMmLOWMLmA84gxgncIH1l	"lybbis,"	nan	0	1
+7j53C7yJCZE5gGYONiu53rrSinShNVFbcWadRWZw	"marlboro,"	"philip_morris, nan, nan"	0	1
+PDTjMw0kHdZB8KpRY0IWV6vRJYGg9yHKQoD6Rwto	"marlboro,"	"marlboro, philip_morris"	1	1
+aQCGasfi61smY4Hu14QTpREsGHmujDFWXWzsjnVM	"mcdonalds,"	nan	0	1
+IMG_3931	"mcdonalds,"	nan	0	1
+seThJe9ov27mGOABrAoMS8v8dxiUCmJY5HnbpgMA	"mcdonalds,"	nan	0	1
+dGuHOS70bk62LSSmDlhu1sadOVLrqWJLaCOTLk9o	"mcdonalds,big gulp,seven eleven,"	nan	0	3
+f1Nzx8QTyVH0ptKN6B3my3kq9jCESpgdpSdp1ShI	"mcdonalds,google,"	nan	0	2
+IMG_8893	"mcdonalds,kirkland,"	"nan, nan"	0	2
+IMG_3572	"mentos,"	nan	0	1
+RE6BKi0UxBu8hWVzrBKpAk3i7eZrIroQCR5rlnHA	"michelob,"	michelob	1	1
+uK60ZIvj439rDQhtcrDdZ5adnnwYZOnHLRB0jj8E	"michelob,"	nan	0	1
+IMG_3848	"miller,"	"modelo, nan"	0	1
+qj7i8w380fYUCGS4oO9YjUuqa0JGIy700ajPpqiG	"mnm,"	nan	0	1
+IMG_6623	"modelo,"	nan	0	1
+IMG_7346	"modelo,"	modelo	1	1
+ZlbBeVYQJFmARnOoP4xVvuYnCpqKkZQGJoKOBVR9	"modelo,"	"modelo, modelo"	1	1
+Zw1CDxqtdI7uq9jZTmzIpjKhN8ayXv2EPfA6jdk6	"modelo,modelo,modelo,modelo,modelo,modelo,modelo,modelo,modelo,modelo,modelo,modelo,modelo,"	"modelo, modelo, modelo"	3	13
+IMG_3209	"modelo,modelo,shasta,"	"nan, nan"	0	3
+wcCht5DhKNMpranLy3uvSGJevHqDUmYy2TO4FvoH	"monster,corona,corona,redbull,"	"corona, corona, modelo"	2	4
+0orY3PkxVw6mPVeft3clwqWJrhiDlY03afoz33ZA	nan	nan	1	1
+1i1eXWTp6MrJwU05wKgvAr2XT3p35LPPLKJDZnNM	nan	nan	1	1
+3E3y1cX5WaUTToWvBLkdJn0f8KZEyvxenfdsgyoD	nan	nan	1	1
+3xbw3ev98S6apR1DddnC67sTSIdR7Es9m6lX42Xo	nan	nan	1	1
+57eSEoXZtA9vkylCOmzPUH8aLOxvai6trLiAkar7	nan	nan	1	1
+5SEAlrU4Q8CP0bz20bXjeSgBFGhsVLkSVB5fZODG	nan	nan	1	1
+9FJ16qWdxoYcODg3EBcOwSbw85pp5KPyG7yjjgkd	nan	nan	1	1
+a9yirboNYyKXPdVuOjSUCXJItY6SuBSJVunZik0u	nan	nan	1	1
+aBr1HRbbyzFgJTcl84Aq9R7JOuFhnqMOmE7XnXwO	nan	nan	1	1
+AGEhjb1hcL8Sw4qbRwuNRpg0fo4ydzrT6jeWODqK	nan	nan	1	1
+Aw3F8NhJa7lT9z5MfNPnA0qz6Hzij74SAiMNVI8R	nan	"nan, nan"	0	1
+BlZUZyFlENJ4fOoQIxEiIAvIowbMRdQIqIewQJIW	nan	nan	1	1
+CyHRsTwCouzGxwoVVDX2HqQgJPvjH3c85wJGQuDB	nan	nan	1	1
+dbux1xSKqXFhb0gECFPIAP19Yz6zrHO2YqjtEwSL	nan	nan	1	1
+DKsxD8a4REfwIB0gBJAtcObcAqC07ud06P6fMVpi	nan	nan	1	1
+FX6zkVQKVngOBxt0wPIGsUMBskqptikfVztnYEE1	nan	nan	1	1
+FYZ9WcIXwboV1Ko3ONQY8exzX1C11kaNdzZIN6Jm	nan	nan	1	1
+fzKPgSrbPLCtLByPcUzKVV8TuK2NgVZDH4TaltMs	nan	nan	1	1
+hHS0KRdyLmsrDZRJBu8x4vD1ElDmOrXdaiMAUrmP	nan	"nan, nan"	0	1
+HhSAboXDvcQ2n6G56b1u32NTjzPXzW7SnzzmQbVL	nan	nan	1	1
+HvUXQxrdmuMt0gfZi80fGQW8VwG6fWdC8NA0JjWN	nan	nan	1	1
+iC4f4jKSEBYPuDtt02Sdo5Dk9RJJLRWTHHBNQArx	nan	nan	1	1
+IMG_0498	nan	nan	1	1
+IMG_0702	nan	nan	1	1
+IMG_0791	nan	nan	1	1
+IMG_0826	nan	nan	1	1
+IMG_0968	nan	"nan, nan"	0	1
+IMG_0995	nan	nan	1	1
+IMG_1223	nan	nan	1	1
+IMG_1254	nan	nan	1	1
+IMG_1268	nan	nan	1	1
+IMG_1330	nan	heineken	0	1
+IMG_1449	nan	heineken	0	1
+IMG_1715	nan	nan	1	1
+IMG_1725	nan	nan	1	1
+IMG_1727	nan	nan	1	1
+IMG_1742	nan	heineken	0	1
+IMG_1756	nan	nan	1	1
+IMG_1919	nan	nan	1	1
+IMG_1935	nan	nan	1	1
+IMG_2075	nan	nan	1	1
+IMG_2105	nan	nan	1	1
+IMG_2192	nan	nan	1	1
+IMG_2195	nan	nan	1	1
+IMG_2497	nan	heineken	0	1
+IMG_2498	nan	"heineken, nan"	0	1
+IMG_2784	nan	nan	1	1
+IMG_3016	nan	corona	0	1
+IMG_3096	nan	"heineken, nan"	0	1
+IMG_3150	nan	busch_light	0	1
+IMG_3760	nan	nan	1	1
+IMG_3762	nan	nan	1	1
+IMG_3867	nan	nan	1	1
+IMG_4097	nan	nan	1	1
+IMG_4194	nan	nan	1	1
+IMG_4245	nan	nan	1	1
+IMG_5473	nan	nan	1	1
+IMG_5480	nan	nan	1	1
+IMG_6055	nan	"deer_park, nan"	0	1
+IMG_6118	nan	nan	1	1
+IMG_6162	nan	nan	1	1
+IMG_6372	nan	nan	1	1
+IMG_6481	nan	bud_light	0	1
+IMG_6555	nan	nan	1	1
+IMG_6575	nan	nan	1	1
+IMG_6727	nan	"nan, nan"	0	1
+IMG_6741	nan	nan	1	1
+IMG_6855	nan	monster	0	1
+IMG_6948	nan	nan	1	1
+IMG_7062	nan	nan	1	1
+IMG_7260	nan	nan	1	1
+IMG_7450	nan	nan	1	1
+IMG_7472	nan	nan	1	1
+IMG_7473	nan	nan	1	1
+IMG_7665	nan	"nan, pepsi"	0	1
+IMG_7740	nan	nan	1	1
+IMG_7784	nan	nan	1	1
+IMG_8198	nan	nan	1	1
+IMG_8255	nan	nan	1	1
+IMG_8342	nan	nan	1	1
+IMG_8516	nan	green_mountain	0	1
+IMG_8953	nan	nan	1	1
+IMG_8989	nan	nan	1	1
+IMG_9212	nan	nan	1	1
+IMG_9238	nan	nan	1	1
+IMG_9330	nan	nan	1	1
+IMG_9377	nan	nan	1	1
+IMG_9703	nan	nan	1	1
+inr46wAH6NpCncgGF3B5JCVCjuEncsWegnTcITz5	nan	nan	1	1
+JGwStXcmK0qDdEBchEnf61zxlTGB73izqS3abC0C	nan	nan	1	1
+kd69BB7NHUNTDJrz83SqxiUAQXWSV0PveTJLiQLJ	nan	nan	1	1
+LtyaYp00kQdc1LTUAZYOXPGf96JAhHq0EQmPDzml	nan	nan	1	1
+luQiwy9gO4Ml4Sst0vYXlgOng70DdMRz66CkbuK3	nan	nan	1	1
+LuYlRPwSkXpvKw5up9eaw0oDU8qzDcWLsMJdn9Us	nan	nan	1	1
+MBzOdboHDQOQq7eVPZ1gQuZQkz2A7mN0I4ilInT4	nan	nan	1	1
+MnBqghhOgmGP9LMPuR3Vxjt7NHz8uI8nRciTA9Sv	nan	nan	1	1
+mPwm0pCRbSC7QD0Z9Vwt9cW5ok30cQBAQqyxwzBL	nan	nan	1	1
+MQrbu5MLkj1uPpofjjvwAPnl3pksDNjKsGcEkmLt	nan	nan	1	1
+Mvk93Vvas1LSBdsZTZhGOQZGmYwr2uuq7Cgp7dsf	nan	nan	1	1
+N9RNIZj3jVU8uj3i7YJ4hjPv2b7DQLmJ4UDmcoGt	nan	nan	1	1
+ncv5Tnw31cH1rRq4SbNZ0Sl9t3sBzGDUDFscL3z3	nan	nan	1	1
+nQFJaMdcdoW8YyNB66E67STcv4MAF7U1xPp6JC68	nan	nan	1	1
+O7E19KjgRtwh4VuWuZTe8aOcVh009hVWoedZ2jcB	nan	nan	1	1
+ouIlxSDaAeAQyiNDRNnDRAthmJZSdflGP8sXXKly	nan	nan	1	1
+p9JAdf7EYmeXj5V4wKAkrvmyYyMy84EVDTjXvUd1	nan	nan	1	1
+PfjWz9Ggc1WyvY8hC1xWZrMfCB7BKIjOFXkVEaxU	nan	nan	1	1
+QYLBnFRvdk2lHplRNZneGz37na6pYFEp2LXivJLx	nan	nan	1	1
+RiZJWMqHNqAN8t5Ls83dxIOnwpxeiADTJnpp1SFp	nan	nan	1	1
+rNQhI3XYLL0SzZRVzzT9Wr4vjxZrBa7HBoNtPOB6	nan	corona	0	1
+rwaBb4vgHZj5ic8EDLY8peZQR77InmTEeuQbvZox	nan	nan	1	1
+S0jgUnBdm7Be8zQmF09gDx0pu2v0yX16yjfDEFix	nan	nan	1	1
+sUViHDlunkA1Miwy4ODzvKawePDiJPV7NUz45rHo	nan	nan	1	1
+svzDWj8aw6Dnzydz5N1gb1DhEIBzDkxDYYTatDFb	nan	heineken	0	1
+tg76RO7S96n40IOyzPmxwbZzjazYQ9wRfofz5Pdl	nan	nan	1	1
+tHs6OST9yklMBfNI2hQDoMirFruZEzEYiLdvbylx	nan	nan	1	1
+tP8w34gzUDdMNl8RIFi7zw5QFqDLJJmdbEKxM3ln	nan	nan	1	1
+U40k2QagwMukiyd9JaBzCYiV87Ha9z1tTyGfYCJ4	nan	nan	1	1
+UiPo8McAxi2M2ENI5BloDxZIXC5atsTdtPjxc8o9	nan	nan	1	1
+UXeEinyBcr79cg8K73xinjGAXXxcbfQWSl7qSsVY	nan	nan	1	1
+VDQvJ34hokVKavOytizY3sg8qefOSEeJYU74GCui	nan	nan	1	1
+W1m2LrFECd1nRjcPyEzKpJvQ00B0YO1Zu1xAXJTx	nan	nan	1	1
+WEfQmsDG6jI6MH1vPRln6GRhUbVOf9dcqXlAgSii	nan	nan	1	1
+WlfhFgIjkJtdMS5LIk4DOULG2bjCpmZUDaxURSLK	nan	nan	1	1
+wPuLNtjiyMatGz5vCsFDwEfC8usHNzVWWe9sYyvU	nan	nan	1	1
+wq0pYiz5ygdinqyRqmlGLQ6VeP07JPvZel1ny97F	nan	nan	1	1
+XgrKgAjWlgyOjLM6HSwxBQL3vP8rWk1vfIf1l6Sk	nan	nan	1	1
+xoZyrHIdNGg7n7yvPoJfKrqlcHj0Dw5BFT67Uq87	nan	"nan, nan"	0	1
+y8uVhK5SmnT358kqv77EaD42WAQakTLtPxh7cPG6	nan	lays	0	1
+z8aXJgaWPLXxG4fz573nVPly1vH8yCtfqXcAefog	nan	nan	1	1
+zXXSJH4KZ9hwXsNZdbHOSFoVxN6FGMJJHy8NMVzT	nan	nan	1	1
+IMG_5774	"natural ice,"	bud_light	0	1
+6fi6JeuBf5GdTOkZjMUKNuz7PjNtiiXH4MuoBjjG	"natural ice,kool-aid,"	"heineken, natural_ice"	1	2
+A2oXgFhUPHjhB74giiXVXCBKdbRPGw6khKTpGK40	"new amsterdam,"	nan	0	1
+IMG_9509	"niagara,"	nan	0	1
+fiHHHdiDQa9Bc8vNKf4RcYENJX17heFMQmZqnait	"oreo,"	"nan, nan"	0	1
+IMG_8921	"pabst blue ribbon,"	nan	0	1
+naSBlJx8EbBtUZb0ns5rut5h38oLkn0azlU9hXQ5	"pabst blue ribbon,"	pbr	1	1
+oUOyQQ2A5m1gVLAHKAqGNcCUDpQbxuB2aHkBJIBk	"pacifico,"	pacifico	1	1
+jRfz23UDZ8n5Tz23MGuke5n3KgeHUefH7e2DadpK	"parliament,"	nan	0	1
+vnQXvgwgh8sIVasQa0jTLtDxGjTGlB1w3HeptYR7	"peaceful fruits,"	nan	0	1
+IMG_2944	"poland spring,deer park,poland spring,poland spring,deer park,kirkland,kirkland,kirkland,michelob,pampers,"	"nan, deer_park"	1	10
+RBitme0b9ssc4J9PCW7MwWFkQS0bEFN15fEykShc	"popeyes,"	nan	0	1
+IMG_6065	"puraqua,"	"nan, nan"	0	1
+9T0MjV6N1D0PYu6T13qpyXphRYeBzMUZwVTzxRGx	"ross ouzo,"	nan	0	1
+frUOJlukN82dhUz08pZvuEjIgViW1guoDQGxilHJ	"sams club,"	nan	0	1
+cP8PErRlvYmRxJriBUtcoGeayxixaWGlbgrQsNaW	"samuel adams,samuel adams,coors,"	nan	0	3
+x6DnRuMFNm8kfF2PmdBk52LkBdvVeLjT5CEd7s4t	"sanpple,"	nan	0	1
+fQtvV3jqTx3sZQ1D8wRGHR3LlByL0XhsH18kLQ3E	"seven eleven,"	bud_light	0	1
+IMG_6420	"seven eleven,"	nan	0	1
+IMG_7709	"seven eleven,"	"nan, nan"	0	1
+9OU0u9rKKlC8mjD2JCic2obGk60gKvmbLMfuCauO	"seven eleven,seven eleven,dorritos,"	"seven_eleven, nan"	1	3
+IMG_5833	"silver falls,"	nan	0	1
+r5M3QlPZHyeJ936u1uA8kFb0OsIDv4ijkJmbnkCn	"slurpee,"	"nan, nan"	0	1
+IMG_8649	"smirnoff,"	nan	0	1
+86SrVUvwVbqwB6uAOIebwXpDDABaRGYmRh1pBCgn	"snickers,"	nan	0	1
+IMG_9370	"sprite,"	nan	0	1
+e0NVXxlAE6cgtmsAmHZAMQGE0TlBRSeHikgR3lXp	"turkey hill,"	nan	0	1
+M3JBcpjpLamkQdLvCpIsPlu1z2G9v6pHKuLipRpW	"twix,"	twix	0	1
+IMG_8486	"two ball,"	nan	0	1
+6BqLZ0NT7wLoV73oBMXSguAH7Ei5hGQQ0vdao2iT	"vitamin,"	nan	0	1
+IMG_6849	"welchs,"	"nan, modelo"	0	1
+dVnzJb91eZ05sGSRaPjIvapDOK0vT9diCiVv2wak	"yoohoo,"	nan	0	1
+			209	440
