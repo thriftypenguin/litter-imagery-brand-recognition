fe55e8b070d828e5c59b0488cb3d36f280656d03
nickjohnson <nickjohnson@berkeley.edu>
2024-08-04 14:28:26 -0400

    cleaning up merge conflict

--- litter-logo-lambda/lambda_function.py
+++ litter-logo-lambda/lambda_function.py
@@ -1,50 +1,3 @@
-<<<<<<< HEAD
-import json
-import psycopg2
-import boto3
-import os
-
-db_name = os.environ['DB_NAME']
-PASSWORD = os.environ['PASSWORD']
-RDS_HOST = os.environ['RDS_HOST']
-RDS_PORT = os.environ['RDS_PORT']
-user_name = os.environ['USER_NAME']
-
-aws_pg_params = {
-    "host": RDS_HOST,
-    "database": "postgres",
-    "user": "postgres",
-    "password": PASSWORD,
-    "port": RDS_PORT
-}
-
-connection_success = 1
-
-try:
-    conn = psycopg2.connect(**aws_pg_params)
-except psycopg2.Error as e:
-    print(e)
-    connection_success = 0
-
-def lambda_handler(event, context):
-    
-    recieved_name = event['queryStringParameters']['brand_name']
-    query = f'''
-    SELECT * 
-    FROM brand as br
-    WHERE br.name = '{recieved_name}';
-    '''
-    cur = conn.cursor()
-    cur.execute(query)
-    # Get list of tuples for all fields
-    
-    response_list = cur.fetchall()
-    return {
-        'statusCode': 200,
-        'body': json.dumps({'brand_name':response_list[0][1], 'brand_score':response_list[0][2]})
-    }
-
-=======
 import json
 import psycopg2
 import boto3
@@ -139,5 +92,3 @@ def lambda_handler(event, context):
                             'confidence': conf
                             })
     }
-
->>>>>>> main
